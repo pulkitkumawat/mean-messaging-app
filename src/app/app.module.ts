fb46e86ebcd2cc7e27a73d68908f96fb9599c4e6
Pulkit Kumawat <31435@ITCINFOTECH.COM>
2020-04-25 02:54:14 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {
+  MatToolbarModule,
+  MatGridListModule,
+  MatCardModule,
+  MatInputModule,
+  MatProgressSpinnerModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatTabsModule,
+  MatIconModule,
+  MatListModule,
+} from "@angular/material";
+import { FormsModule } from "@angular/forms";
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatCardModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    FormsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatTabsModule,
+    MatIconModule,
+    MatListModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
